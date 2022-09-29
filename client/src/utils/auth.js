942f45530855cfe390c66a7f71479c995f613501
JT Gutierrez <j.tgutierrez@JTs-MacBook-Pro.lan>
2022-09-28 21:46:11 -0500

    updated auth.js to fix comment

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,12 +1,12 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const secret = 'mysecretsshhhhh';
-// const expiration = '2h';
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
-// module.exports = {
-//   signToken: function({ username, email, _id }) {
-//     const payload = { username, email, _id };
+module.exports = {
+signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   }
-// };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+};

f21021ec4a31b565f53532b09a2543d48f6e687e
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-28 22:24:42 -0500

    commentform styling still needs to be changed once i see what it looks like

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_COMMENT } from "../../utils/mutations";
 
@@ -29,23 +29,37 @@ const CommentForm = ({ dreamId }) => {
   };
 
   return (
-    <div>
-      <p className="">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Leave a comment
+        </h2>
+      </div>
+      <p
+        className={`m-2 ${characterCount === 280 || error ? "text-error" : ""}`}
+      >
         Character Count {characterCount}/280
-        {error && <span className="">Something went wrong...</span>}
+        {error && <span className="m-3">Something went wrong...</span>}
       </p>
-      <form className="" onSubmit={handleFormSubmit}>
-        <textarea
-          placeholder="Leave a comment to this dream"
-          value={commentBody}
-          className=""
-          onChange={handleChange}
-        ></textarea>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
+          <form className="mb-14 space-y-8" onSubmit={handleFormSubmit}>
+            <textarea
+              placeholder="Leave a comment to this dream"
+              value={commentBody}
+              className="mt-6 text-center text-3xl font-extrabold text-gray-900"
+              onChange={handleChange}
+            ></textarea>
 
-        <button className="" type="submit">
-          Submit
-        </button>
-      </form>
+            <button
+              className="mt-10 w-full flex justify-center py-4 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
       {error && <div>Something went wrong </div>}
     </div>
   );

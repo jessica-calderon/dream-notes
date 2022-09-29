05c93d53d119a663e2cc0670841791680f195893
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-29 18:27:36 -0500

    need to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloProvider,
@@ -9,6 +10,7 @@ import {
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";

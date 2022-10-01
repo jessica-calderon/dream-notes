3b82d82fafa17dc9e3d44a2c64f39f90f0f098bc
Jessica Calderon <calderonjessica13@yahoo.com>
2022-10-01 01:09:01 -0500

    start custom css

--- client/src/components/DreamList/index.js
+++ client/src/components/DreamList/index.js
@@ -7,32 +7,37 @@ const DreamList = ({ dreams, title }) => {
   }
 
   return (
-    <div>
-      <h3>{title}</h3>
+    <div className="container">
+      <div className="w-100 mt-3 ml-3">
+            <h3 className="text-large">{title}</h3></div>
+      <div className="row min-80-vw cards mt-3">
       {dreams &&
         dreams.map((dream) => (
-          <div key={dream._id} className="">
-            <p className="">
+          <div key={dream._id} className="card flex card-1">
+            <h2 className="card__footer">
               <Link
                 to={`/profile/${dream.username}`}
                 style={{ fontWeight: 700 }}
-                className="text-light"
+                className="text-light card__link mt-2"
               >
                 {dream.username}
               </Link>{" "}
               dreamed on {dream.createdAt}
-            </p>
-            <div className="">
+            </h2>
+            <div className="card__text">
+            <h2 className="card__text__link">
               <Link to={`/dream/${dream._id}`}>
-                <p>{dream.dreamText}</p>
-                <p className="mb-0">
+                <h2 className="">{dream.dreamText}</h2>
+                <p className="mb-0 card__footer__link">
                   Comments: {dream.commentCount} || Click to{" "}
                   {dream.commentCount ? "see" : "start"} dreams now!
                 </p>
               </Link>
+              </h2>
             </div>
           </div>
         ))}
+    </div>
     </div>
   );
 };

a0f62393beba794953cdae515fc73be067ddcb3d
Jessica Calderon <calderonjessica13@yahoo.com>
2022-10-01 03:01:56 -0500

    updating styling

--- client/src/components/DreamList/index.js
+++ client/src/components/DreamList/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pillow from "../../assets/img/1.png";
+import nightMoon from "../../assets/img/8.png"
 
 const DreamList = ({ dreams, title }) => {
   if (!dreams.length) {
-    return <h3>No Dreams Found</h3>;
+    return <h3>No Dreams Found 😢</h3>;
   }
 
   return (
     <div className="container">
       <div className="w-100 mt-3 ml-3">
-            <h3 className="text-large">{title}</h3></div>
+            <h3 className="text-large text-beige">
+              <img src={nightMoon} alt="Moon, clouds, and stars" height="75px;" className="mr-2 pt-2" />{title}</h3></div>
       <div className="row min-80-vw cards mt-3">
       {dreams &&
         dreams.map((dream) => (
@@ -18,7 +21,7 @@ const DreamList = ({ dreams, title }) => {
               <Link
                 to={`/profile/${dream.username}`}
                 style={{ fontWeight: 700 }}
-                className="text-light card__link mt-2"
+                className="text-dark card__link mt-3"
               >
                 {dream.username}
               </Link>{" "}
@@ -27,10 +30,13 @@ const DreamList = ({ dreams, title }) => {
             <div className="card__text">
             <h2 className="card__text__link">
               <Link to={`/dream/${dream._id}`}>
-                <h2 className="">{dream.dreamText}</h2>
-                <p className="mb-0 card__footer__link">
+              <h2 className="">
+                <img src={pillow} alt="Sleeping pillow" width="40px" className="mr-2"/> 
+                {dream.dreamText}
+                </h2>
+                <p className="mb-0 text-med card__footer__link">
                   Comments: {dream.commentCount} || Click to{" "}
-                  {dream.commentCount ? "see" : "start"} dreams now!
+                  {dream.commentCount ? "see more dreams" : "start dreaming"} now!
                 </p>
               </Link>
               </h2>
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,26 +12,31 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary p-4">
+    <header className="bg-med p-4">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <img src={brand} className="mt-3 mr-3" alt="Brand Logo" width="150px;"/>
           {/* <h1 className='6xl'>Dream Notes</h1> */}
         </Link>
 
-        <nav className="text-center">
+        <nav className="text-center mx-auto">
           {Auth.loggedIn() ? (
             <>
+              <span className="nav-link">
               <Link to="/profile">Me</Link>
+              < br/>
               <a href="/" onClick={logout}>
                 Logout
               </a>
+              </span>
             </>
           ) : (
             <>
+              <span className="nav-link">
               <Link to="/login">Login</Link>
               <br/>
               <Link to="/signup">Signup</Link>
+              </span>
             </>
           )}
         </nav>

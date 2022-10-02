bf55c400f4ce012c4e064252d7ac525f07c844d3
Jessica Calderon <calderonjessica13@yahoo.com>
2022-10-02 03:16:44 -0500

    fix mongoose db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dream-note",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dream-notes",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

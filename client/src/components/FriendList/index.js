9c474b46e8883fd8d1aaa07483185dc8d26e0f49
unknown <mcsalazar122@gmail.com>
2022-10-03 23:08:11 -0500

    update

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import darkcloud from "../../assets/img/2.png";
 
 const FriendList = ({ friendCount, username, friends }) => {
   if (!friends || !friends.length) {
-    return <div className="container w-100"><div className="card-4 m-4 p-2"><p className="text-dark p-3">{username}, make some friends!</p></div></div>;
+    return <div className="container w-100"><div className="card-4 m-4 p-2">  <img src={darkcloud} alt="dark cloud mobile" width="50px" className="mr-2"/> <p className="text-dark p-3">{username}, add a friend to share dreams</p></div></div>;
   }
 
   return (

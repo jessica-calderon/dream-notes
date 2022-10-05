d1174b6c897b0113d65895744c67c54085cae208
JT Gutierrez <j.tgutierrez@JTs-MacBook-Pro.lan>
2022-10-05 17:05:50 -0500

    updated serviceworker.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+const service = serviceWorker.register();
 ReactDOM.render(
   <React.StrictMode>
     <App />
